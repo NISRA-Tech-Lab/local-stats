b05dac78db12d22505292f3c10553a0f2ab80f69
brianq-nisra <brian.quinn@nisra.gov.uk>
2024-04-18 15:57:59 +0100

    Education accordion brought back in

--- src/routes/[code]/+page.svelte
+++ src/routes/[code]/+page.svelte
@@ -920,7 +920,7 @@
 					explored in the <a href='https://explore.nisra.gov.uk/area-explorer-2021/N92000002/'>Census Area Explorer</a> and the <a href='https://build.nisra.gov.uk/en/'>Flexible Table Builder</a>.</p>"
 	/>
 
-		<!-- <Accordion
+		<Accordion
 		id = "education"
 		img = "nisra-taxonomy-icon-child-education-skills.png"
 		heading = "Education"
@@ -950,7 +950,7 @@
 						id: "teachers",
 						content: 		
 										"<p><span class='text-big' style='font-size: 1.8em'>"  + 
-											(data.place.data.ClassSize.value).toLocaleString() +"</span> puils per teacher</p>"
+											(check("ClassSize.value")).toLocaleString() +"</span> puils per teacher</p>"
 ,
 					
 					show: ["ni", "lgd"]},
@@ -973,7 +973,7 @@
 					show: ["ni", "lgd"]},
 							}}
 		more = "<p>The <a href='https://www.nisra.gov.uk/statistics/children-education-and-skills/school-education-statistics'>Department of Education</a> publishes statistics on <a href='https://www.education-ni.gov.uk/articles/school-enrolments-overview'>school enrolments</a>, <a href='https://www.education-ni.gov.uk/articles/school-performance'>school performance</a>, <a href='https://www.education-ni.gov.uk/articles/school-leavers'>school leavers</a>, qualifications and destinations, <a href='https://www.education-ni.gov.uk/articles/pupil-attendance'>pupil attendance</a>, suspensions and expulsions, school meals and <a href='https://www.education-ni.gov.uk/articles/education-workforce'>education workforce</a>. The <a href='https://www.nisra.gov.uk/statistics/children-education-and-skills/higher-and-further-education-and-training-statistics'>Department for the Economy</a> publishes <a href='https://www.economy-ni.gov.uk/topics/statistics-and-economic-research/higher-education-statistics-and-research'>Higher</a> and <a href='https://www.economy-ni.gov.uk/topics/statistics-and-economic-research/further-education-statistics-and-research'>Further</a> education and <a href='https://www.economy-ni.gov.uk/articles/training-success-statistics'>training</a> statistics. The <a href='https://www.nisra.gov.uk/statistics/census'>2021 census</a> collected data on qualifications which can be explored in the <a href='https://explore.nisra.gov.uk/area-explorer-2021/N92000002/'>Census Area Explorer</a> and the <a href='https://build.nisra.gov.uk/en/'>Flexible Table Builder</a>.</p>"
-	/> -->
+	/>
 
 
 	<Accordion
