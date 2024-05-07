d3293b8104edd68bb57bf1800e779a8e29252305
brianq-nisra <brian.quinn@nisra.gov.uk>
2024-05-07 17:25:54 +0100

    Updates

---
+++ _app/immutable/nodes/4.7c01b862.js
@@ -0,0 +1,126 @@
+import{S as W,i as Y,s as F,k as m,q as U,a as D,y as j,l as f,m as b,r as Z,h as p,c as w,z as J,n as N,b as M,C as l,A as K,g as Q,d as X,B as tt,G as C,H as rt,D as nt,I as ot,w as G}from"../chunks/index.baf6a862.js";import{S as et}from"../chunks/Section.6106804d.js";import{b as S}from"../chunks/paths.041ad106.js";function it(u){let a,e,o,i,c,g,t,r,s,x,A,y,h,_,E,n,k,L;return{c(){a=m("body"),e=m("div"),o=m("div"),i=m("h2"),c=U("Enter your postcode to find its location"),g=D(),t=m("form"),r=m("input"),s=D(),x=m("button"),A=U(" "),y=D(),h=m("div"),_=D(),E=m("style"),n=U(`form {\r
+			position: relative;\r
+			display: inline-block;\r
+			width: 100%;\r
+			margin-top: 15px;\r
+		}\r
+\r
+		.container {\r
+			max-width: 600px;\r
+			margin: 0 auto;\r
+			padding: 20px;\r
+		}\r
+\r
+		h2 {\r
+			text-align: left;\r
+			color: #333;\r
+			font-size: 1.5em;\r
+			line-height: 1.3;\r
+			margin: 0px \r
+		}\r
+\r
+		input[type="text"] {\r
+			width: 100%;\r
+			padding: 10px 40px 10px 10px;\r
+			margin-bottom: 10px;\r
+			border: 2px solid #00205b !important;\r
+			border-radius: 0px;\r
+			background-color: #f5f5f6;\r
+			color: #00205b;\r
+		}\r
+\r
+		input[type="text"]:focus-visible {\r
+			border: 2px solid #00205b;\r
+			border-radius: 0px;\r
+		}\r
+\r
+		button {\r
+			background-color: #00205b;\r
+			background-repeat: no-repeat;\r
+			background-position: center;\r
+			border-radius: 0px;\r
+			color: #fff;\r
+			border: none;\r
+			cursor: pointer;\r
+			background-image: url("/img/search.svg");\r
+			position: absolute;\r
+			top: 0;\r
+			right: 0;\r
+			width: 40px;\r
+			height: 46px;\r
+		}\r
+\r
+		button:focus {\r
+			background-color: #00205b;\r
+		}\r
+\r
+\r
+		#result {\r
+			margin-top: 20px;\r
+			padding: 10px;\r
+			border: 1px solid #ddd;\r
+			border-radius: 4px;\r
+		}`),this.h()},l(d){a=f(d,"BODY",{});var v=b(a);e=f(v,"DIV",{class:!0});var H=b(e);o=f(H,"DIV",{class:!0});var $=b(o);i=f($,"H2",{});var I=b(i);c=Z(I,"Enter your postcode to find its location"),I.forEach(p),g=w($),t=f($,"FORM",{});var T=b(t);r=f(T,"INPUT",{type:!0,placeholder:!0}),s=w(T),x=f(T,"BUTTON",{});var P=b(x);A=Z(P," "),P.forEach(p),T.forEach(p),y=w($),h=f($,"DIV",{}),b(h).forEach(p),$.forEach(p),H.forEach(p),v.forEach(p),_=w(d),E=f(d,"STYLE",{});var R=b(E);n=Z(R,`form {\r
+			position: relative;\r
+			display: inline-block;\r
+			width: 100%;\r
+			margin-top: 15px;\r
+		}\r
+\r
+		.container {\r
+			max-width: 600px;\r
+			margin: 0 auto;\r
+			padding: 20px;\r
+		}\r
+\r
+		h2 {\r
+			text-align: left;\r
+			color: #333;\r
+			font-size: 1.5em;\r
+			line-height: 1.3;\r
+			margin: 0px \r
+		}\r
+\r
+		input[type="text"] {\r
+			width: 100%;\r
+			padding: 10px 40px 10px 10px;\r
+			margin-bottom: 10px;\r
+			border: 2px solid #00205b !important;\r
+			border-radius: 0px;\r
+			background-color: #f5f5f6;\r
+			color: #00205b;\r
+		}\r
+\r
+		input[type="text"]:focus-visible {\r
+			border: 2px solid #00205b;\r
+			border-radius: 0px;\r
+		}\r
+\r
+		button {\r
+			background-color: #00205b;\r
+			background-repeat: no-repeat;\r
+			background-position: center;\r
+			border-radius: 0px;\r
+			color: #fff;\r
+			border: none;\r
+			cursor: pointer;\r
+			background-image: url("/img/search.svg");\r
+			position: absolute;\r
+			top: 0;\r
+			right: 0;\r
+			width: 40px;\r
+			height: 46px;\r
+		}\r
+\r
+		button:focus {\r
+			background-color: #00205b;\r
+		}\r
+\r
+\r
+		#result {\r
+			margin-top: 20px;\r
+			padding: 10px;\r
+			border: 1px solid #ddd;\r
+			border-radius: 4px;\r
+		}`),R.forEach(p),this.h()},h(){N(r,"type","text"),N(r,"placeholder","Enter postcode..."),N(o,"class","container div-grey-box"),N(e,"class","row")},m(d,v){M(d,a,v),l(a,e),l(e,o),l(o,i),l(i,c),l(o,g),l(o,t),l(t,r),u[3](r),l(t,s),l(t,x),l(x,A),l(o,y),l(o,h),u[4](h),M(d,_,v),M(d,E,v),l(E,n),k||(L=[C(x,"click",u[2]),C(t,"submit",rt(u[2]))],k=!0)},p:nt,d(d){d&&p(a),u[3](null),u[4](null),d&&p(_),d&&p(E),k=!1,ot(L)}}}function at(u){let a,e,o,i,c,g;return c=new et({props:{column:"wide",$$slots:{default:[it]},$$scope:{ctx:u}}}),{c(){a=m("nav"),e=m("a"),o=U("Home"),i=D(),j(c.$$.fragment),this.h()},l(t){a=f(t,"NAV",{});var r=b(a);e=f(r,"A",{href:!0});var s=b(e);o=Z(s,"Home"),s.forEach(p),r.forEach(p),i=w(t),J(c.$$.fragment,t),this.h()},h(){N(e,"href",S+"/")},m(t,r){M(t,a,r),l(a,e),l(e,o),M(t,i,r),K(c,t,r),g=!0},p(t,[r]){const s={};r&35&&(s.$$scope={dirty:r,ctx:t}),c.$set(s)},i(t){g||(Q(c.$$.fragment,t),g=!0)},o(t){X(c.$$.fragment,t),g=!1},d(t){t&&p(a),t&&p(i),tt(c,t)}}}function B(u){return u.replace(/\s/g,"").toUpperCase()}function st(u,a,e){let o,i;function c(){const r=B(o.value);if(r===""){alert("Please enter a postcode.");return}fetch("https://raw.githubusercontent.com/NISRA-Tech-Lab/nisra-geog-explorer/main/search_data/cpd_light_with_lat_long_2.csv").then(s=>s.text()).then(s=>{const x=s.split(`
+`);let A=!1,y="<p>",h="<ul>",_="<ul>";x.forEach(E=>{const n=E.split(","),k=B(n[0]);if(k===r){const L=n[4].trim(),d=n[5].trim();n[6].trim();const v=n[7].trim(),H=n[8].trim(),$=n[9].trim();n[10].trim();const I=n[11].trim(),T=n[17].trim(),P=n[18].trim(),R=n[19].trim(),V=n[20].trim(),z=n[21].trim(),O=n[28].trim(),q=n[29].trim();y+=`<strong>Postcode:</strong> ${k.slice(0,-3)} ${k.substr(-3)}`,h+=`<li><strong>Local Government District:</strong> <a href="${S}/${L}" target="_blank">${d}</a></li><li><strong>District Electoral Area:</strong> <a href="${S}/${H}" target="_blank">${$}</a></li><li><strong>Super Data Zone:</strong> <a href="${S}/${V}" target="_blank">${z}</a></li><li><strong>Data Zone:</strong> <a href="${S}/${P}" target="_blank">${R}</a></li>`,_+=`<li><strong>Urban / Rural:</strong> ${q}</li><li><strong>Settlement:</strong> ${O}</li><li><strong>Health and Social Care Trust:</strong> ${T}</li><li><strong>Assembly Area Name:</strong> ${I}</li><li><strong>Ward Name:</strong> ${v}</li>`,A=!0}}),y+="</p>",h+="</ul>",_+="</ul>",A?e(1,i.innerHTML=y+"<p>Geographies in Area Explorer</p>"+h+"<p>Geographies not in Area Explorer</p>"+_,i):e(1,i.innerHTML="Postcode not found.",i)}).catch(s=>{console.error("Error fetching data:",s),alert("Error fetching data. Please try again.")})}function g(r){G[r?"unshift":"push"](()=>{o=r,e(0,o)})}function t(r){G[r?"unshift":"push"](()=>{i=r,e(1,i)})}return[o,i,c,g,t]}class dt extends W{constructor(a){super(),Y(this,a,st,at,F,{})}}export{dt as component};
