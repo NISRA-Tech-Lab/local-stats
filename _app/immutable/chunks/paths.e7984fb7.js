5ad41309d2eb47566612475035aa051e84aa7c9e
MaireBrolly <78856621+MaireBrolly@users.noreply.github.com>
2024-05-17 14:10:27 +0100

    Updates

--- _app/immutable/chunks/paths.e7984fb7.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1tu4u55)==null?void 0:s.base)??"/nisra-geog-explorer";var e;const a=((e=globalThis.__sveltekit_1tu4u55)==null?void 0:e.assets)??"https://NISRA-Tech-Lab.github.io/nisra-geog-explorer";export{a,t as b};
