0502e8985a1eb13296de23200623e94d3c8b751f
MaireBrolly <78856621+MaireBrolly@users.noreply.github.com>
2024-08-16 16:50:07 +0100

    reset for scheduler

--- create_jsons/create_JSONs.R
+++ create_jsons/create_JSONs.R
@@ -22,8 +22,8 @@ df_geog_codes_for_loop <-
     substr(code, 1, 3) == "N20" ~ "dz",
     substr(code, 1, 3) == "N21" ~ "sdz"
   )) %>%
-# filter(type %in% geog_types_to_update) %>%
-  filter(type %in% c('ctry', 'lgd', 'dea')) %>%
+ filter(type %in% geog_types_to_update) %>%
+#  filter(type %in% c('ctry', 'lgd', 'dea')) %>%
   arrange(substr(code, 1, 3))
  
  
