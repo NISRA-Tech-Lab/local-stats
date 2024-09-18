d316cc8fee101ac94cef5073f8ac42efd72b07ba
MaireBrolly <78856621+MaireBrolly@users.noreply.github.com>
2024-09-18 16:13:00 +0100

    Updates

---
+++ _app/immutable/chunks/paths.b0b4a9b8.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_nups1n)==null?void 0:s.base)??"/local-stats-dev";var t;const e=((t=globalThis.__sveltekit_nups1n)==null?void 0:t.assets)??"https://NISRA-Tech-Lab.github.io/local-stats-dev";export{e as a,a as b};
