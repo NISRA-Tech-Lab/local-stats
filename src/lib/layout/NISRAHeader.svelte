67d34aa050db42a60b68a7fbfcfcf97d4bd50fa2
jmccallion <jessica.mccallion@nisra.gov.uk>
2025-12-12 13:33:14 +0000

    Tidied up header and footer code

--- src/lib/layout/NISRAHeader.svelte
+++ src/lib/layout/NISRAHeader.svelte
@@ -41,7 +41,7 @@
 							</picture>
 						</a>
 					</div>
-					<div style = "display: flex; flex-direction:column; justify-content: right; align-items: center">
+					 <div class="header-text">
 						<h1 style="font-size: 28px; font-weight: 700;color: #ffffff; line-height: 1em; margin-bottom: 10px;">Northern Ireland Local Statistics Explorer</h1>
 						<span style = "font-size: 24px; line-height: 1.333">Find out how your local area compares to NI</span>
 					</div>
@@ -84,6 +84,13 @@
 	.mtb {
 		margin-top: 10px;
 		margin-bottom: 20px;
+	}
+
+	.header-text {
+		display: flex;
+		flex-direction: column;
+		align-items: left;
+		text-align: left;
 	}
 
 	  
